5e55cdc09e0b5f0ff57a663c007744ac719ac6d0
nicolasjhampton <nicolasjhampton@gmail.com>
2016-05-03 01:04:45 -0700

    Object-Oriented

--- js/pageTurner.js
+++ js/pageTurner.js
@@ -1,24 +1,28 @@
-var pageTurner = (function() {
+var PageTurner = (function($) {
   'use strict';
 
-  var config = {};
+  /*
+   * Constructor function to initialize pageTurner
+   *
+   */
 
-  var studentSearch; // jquery object - default: $('<div class="student-search"></div>')
-  var pageHeader; // jquery object - default: $('.page-header')
-  var currentPage; // jquery object - default: $('.page')
-  var students; // jquery object - default: $('.student-item')
-  var empty; // jquery object - default: $('<h1 class="empty">No Results Found</h1>')
-  var studentList; // jquery object - default: $('.student-list')
-  var input; // jquery object - default: $('<input placeholder="Search for students...">')
+  function pageTurner(config) {
 
-  var elementsToQuery; //an array of css selectors - default: ['h3','span.email']
-  var itemsOnPage; // integer - default: 10
+    if(!config) {config = {};}
+    this.studentSearch = config.studentSearch || $('<div class="student-search"></div>');
+    this.pageHeader = config.pageHeader || $('.page-header');
+    this.currentPage = config.currentPage || $('.page');
+    this.students = config.students || $('.student-item');
+    this.empty = config.empty || $('<h1 class="empty">No Results Found</h1>');
+    this.studentList = config.studentList || $('.student-list');
+    this.input = config.input || $('<input placeholder="Search for students...">');
+    this.elementsToQuery = config.elementsToQuery || ['h3','span.email'];
+    this.itemsOnPage = config.itemsOnPage || 10;
+    this.paginationClass = config.paginationElement || 'pagination';
+    this.paginationElement = config.paginationElement || 'div';
+    this.paginationString = '<' + this.paginationElement + ' class="' + this.paginationClass + '"></' + this.paginationElement + '>';
 
-  var paginationClass; // string of a class name - default: 'pagination'
-  var paginationElement; // string to identify a tag name - default: 'div'
-  var paginationString; // string created from paginationClass and paginationElement
-  // - default: '<' + paginationElement + ' class="' + paginationClass + '"></' + paginationElement + '>';
-
+  }
 
 
   /*
@@ -28,16 +32,19 @@ var pageTurner = (function() {
 
   function search() {
 
-    var query = input.val().toLowerCase().trim(); // Get search value
+    var that = this;
+    var query = that.input.val().toLowerCase().trim(); // Get search value
+    console.log(query);
 
-    var matches = students.filter(function(index) { // Go through all the students
 
-      var that = this; // Store the student item element
+    var matches = this.students.filter(function(index) { // Go through all the students
+
+      var studentItem = this; // Store the student item element
 
       // go through the list of elements in the student item we want to search
-      var results = elementsToQuery.map(function(element) {
+      var results = that.elementsToQuery.map(function(element) {
         // get the text of the element
-        var elementText = $(that).find(element).text().toLowerCase().trim();
+        var elementText = $(studentItem).find(element).text().toLowerCase().trim();
         // If the query matches the element text
         if (elementText.search(query) !== -1) {
           return true; // return a 'matched' result
@@ -51,12 +58,12 @@ var pageTurner = (function() {
 
     });
 
-    var pagination = createPage(0, students, matches); // display page and create menu
-    $('.' + paginationClass).remove(); // Remove the current menu
+    var pagination = that.createPage(0, that.students, matches); // display page and create menu
+    $('.' + that.paginationClass).remove(); // Remove the current menu
 
     if(matches.length !== 0) {
-      if(empty) {empty.remove();} // remove empty message if it's there
-      currentPage.append(pagination); // attach menu
+      if(this.empty) {this.empty.remove();} // remove empty message if it's there
+      this.currentPage.append(pagination); // attach menu
     }
   };
 
@@ -66,7 +73,7 @@ var pageTurner = (function() {
    * Used by createPage
    */
 
-  function displayPage(page, entries, matches) {
+  pageTurner.prototype.displayPage = function(page, entries, matches) {
 
     var itemsOnPage = 10; // number of items to display on a page
     var bottom = page * itemsOnPage; // Bottom of list to display
@@ -77,10 +84,10 @@ var pageTurner = (function() {
     });
 
     if(matches.length === 0) {
-      studentList.hide();
-      currentPage.append(empty); // display empty message
+      this.studentList.hide();
+      this.currentPage.append(this.empty); // display empty message
     } else {
-      studentList.show();
+      this.studentList.show();
       matches.each(function(index){
         // if the entry is in the matches and on this page
         if (index >= bottom && index <= top && matches.index(this) !== -1) {
@@ -98,14 +105,15 @@ var pageTurner = (function() {
    * Used by createPageNavList
    */
 
-  function createPageLink(index, navObj) {
+  pageTurner.prototype.createPageLink = function(index, navObj) {
+    var that = this;
     var pageLink = $('<li><a href="#"></a></li>'); // create a link...
     pageLink.children().text(index + 1).click(function(e) { // Add the link text and on click handler
       // This is what happens when the link is clicked
       e.preventDefault(); // Prevent the link from leaving the page
       $(this).addClass('active'); // Mark it active
       $(this).parent().siblings().children().removeClass('active'); // Remove active class from any other link
-      createPage(index, navObj.entries, navObj.matches); // Change the page
+      that.createPage(index, navObj.entries, navObj.matches); // Change the page
     });
     // If this page is the current page, mark the link as active
     navObj.page === index ? pageLink.children().addClass('active') : null;
@@ -118,11 +126,11 @@ var pageTurner = (function() {
    * Used by createPage
    */
 
-  function createPageNavList(navObj) {
+  pageTurner.prototype.createPageNavList = function(navObj) {
     var navList = $('<ul></ul>'); // Create nav list.
     var navIndex = Math.ceil(navObj.matches.length / 10); // Get the number of pages needed.
     for(var i = 0; i < navIndex; i++) { // for each page...
-      var pageLink = createPageLink(i, navObj); // create a link...
+      var pageLink = this.createPageLink(i, navObj); // create a link...
       navList.append(pageLink); // and attach it to nav list.
     }
     return navList;
@@ -135,43 +143,30 @@ var pageTurner = (function() {
    * and Initialization code
    */
 
-  function createPage(page, entries, matches) {
-    var navObj = displayPage(page, entries, matches);
-    var navList = createPageNavList(navObj);
-    var pagination = $(paginationString);
+  pageTurner.prototype.createPage = function(page, entries, matches) {
+    var navObj = this.displayPage(page, entries, matches);
+    var navList = this.createPageNavList(navObj);
+    var pagination = $(this.paginationString);
     pagination.append(navList);
     return pagination;
   }
 
 
   /*
-   * Public method to initialize pageTurner
-   * Used in script tag at bottom of page
+   * Run method
+   * Used to attach PageTurner object to DOM
+   * and Initialization code
    */
 
-  return {
-    init: function(config) {
-      if(!config) {config = {};}
-      studentSearch = config.studentSearch || $('<div class="student-search"></div>');
-      pageHeader = config.pageHeader || $('.page-header');
-      currentPage = config.currentPage || $('.page');
-      students = config.students || $('.student-item');
-      empty = config.empty || $('<h1 class="empty">No Results Found</h1>');
-      studentList = config.studentList || $('.student-list');
-      input = config.input || $('<input placeholder="Search for students...">');
+  pageTurner.prototype.run =  function() {
+    var that = this;
+    this.input.keyup(search.bind(that));
+    this.studentSearch.append(this.input);
+    this.pageHeader.append(this.studentSearch);
+    var pagination = this.createPage(0, this.students, this.students);
+    this.currentPage.append(pagination);
+  }
 
-      elementsToQuery = config.elementsToQuery || ['h3','span.email'];
-      itemsOnPage = config.itemsOnPage || 10;
+  return pageTurner;
 
-      paginationClass = config.paginationElement || 'pagination';
-      paginationElement = config.paginationElement || 'div';
-      paginationString = '<' + paginationElement + ' class="' + paginationClass + '"></' + paginationElement + '>';
-
-      input.keyup(search);
-      studentSearch.append(input);
-      pageHeader.append(studentSearch);
-      var pagination = createPage(0, students, students);
-      currentPage.append(pagination);
-    }
-  };
-})();
+})($);
