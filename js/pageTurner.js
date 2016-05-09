4a44f13c095c0a101da6f04b9dd45c2cd153b430
nicolasjhampton <nicolasjhampton@gmail.com>
2016-05-08 17:41:07 -0700

    Documentation and performance enhancements added

--- js/pageTurner.js
+++ js/pageTurner.js
@@ -1,15 +1,18 @@
-var PageTurner = (function($) {
+var PageTurner = (function($, window, document) {
   'use strict';
 
-  /*
-   * Constructor function to initialize pageTurner
-   *
-   */
+/*
+ *  PageTurner Version 1.0.0
+ *  Copyright Nicolas James Hampton 2016
+ *  Creates pages out of identical html items
+ *  with built in search
+ *
+ */
 
+  var students;
   var studentSearch;
   var pageHeader;
   var currentPage;
-  var students;
   var empty;
   var studentList;
   var input;
@@ -19,10 +22,67 @@ var PageTurner = (function($) {
   var paginationElement;
   var paginationString;
 
+  var defaults = {
+    "studentSearch": $('<div class="student-search"></div>'),
+    "pageHeader": $('.page-header'),
+    "currentPage": $('.page'),
+    "students": $('.student-item'),
+    "empty": $('<h1 class="empty">No Results Found</h1>'),
+    "studentList": $('.student-list'),
+    "input": $('<input placeholder="Search for students...">'),
+    "elementsToQuery": [
+      'h3',
+      'span.email'
+    ],
+    "itemsOnPage": 10,
+    "paginationElement": 'pagination',
+    "paginationElement": 'div'
+  }
+
+  /*
+   * Constructor function for pageturner object
+   *
+   */
+  function PageTurner(options) {
+    if(!options) {options = {};}
+    var config = $.extend( {}, defaults, options );
+    studentSearch = config.studentSearch;
+    pageHeader = config.pageHeader;
+    currentPage = config.currentPage;
+    students = config.students;
+    empty = config.empty;
+    studentList = config.studentList;
+    input = config.input;
+    elementsToQuery = config.elementsToQuery;
+    itemsOnPage = config.itemsOnPage;
+    paginationClass = config.paginationElement;
+    paginationElement = config.paginationElement;
+    paginationString = '<' + paginationElement + ' class="' + paginationClass + '"></' + paginationElement + '>';
+  }
+
+
+  /*
+   * Run method
+   * Used to attach PageTurner object to DOM
+   * and Initialization code
+   *
+   */
+  pageTurner.prototype.run =  function() {
+    var that = this;
+    
+    input.keyup(search.bind(that));
+    studentSearch.append(input);
+    pageHeader.append(studentSearch);
+
+    var pagination = createPage(0, students, students);
+    currentPage.append(pagination);
+  }
+
 
   /*
    * search method
-   * Used by input keyup (in initialization code)
+   * Used by pageTurner.run initialization method to bind to keyup event
+   *
    */
 
   function search() {
@@ -61,13 +121,21 @@ var PageTurner = (function($) {
   /*
    * displayPage method
    * Used by createPage
+   *
+   * @param pageIndex: Integer - Page number you want to display (starts at 0)
+   *        entries: DOM element array - all possible items
+   *        matches: DOM element array - all currently matching search items
+   *
+   * @returns navObj: Keys: Page
+   *                        entries
+   *                        matches
+   *
    */
-
-  var displayPage = function(page, entries, matches) {
+  var displayPage = function(pageIndex, entries, matches) {
 
     var itemsOnPage = 10; // number of items to display on a page
-    var bottom = page * itemsOnPage; // Bottom of list to display
-    var top = page * itemsOnPage + (itemsOnPage - 1); // Top of list to display
+    var bottom = pageIndex * itemsOnPage; // Bottom of list to display
+    var top = pageIndex * itemsOnPage + (itemsOnPage - 1); // Top of list to display
 
     entries.each(function() {
       $(this).hide();
@@ -86,36 +154,50 @@ var PageTurner = (function($) {
       });
     }
 
-    return {"entries": entries, "page":page, "matches": matches}; // This is the navObj
+    return {"entries": entries, "page":pageIndex, "matches": matches}; // This is the navObj
   };
 
 
   /*
    * createPageLink method
    * Used by createPageNavList
+   *
+   * @param  pageIndex: Integer - number of page starting with 0
+   *         navObj: Keys: Page
+   *                       entries
+   *                       matches
+   *
+   * @returns DOM list item element for a page number link
+   *
    */
-
-  var createPageLink = function(index, navObj) {
+  var createPageLink = function(pageIndex, navObj) {
     var that = this;
+    var pageNumberText = pageIndex + 1;
     var pageLink = $('<li><a href="#"></a></li>'); // create a link...
-    pageLink.children().text(index + 1).click(function(e) { // Add the link text and on click handler
+    // If this page is the current page, mark the link as active
+    navObj.page === index ? pageLink.children().addClass('active') : null;
+    pageLink.children().text(pageNumberText).click(function(e) { // Add the link text and on click handler
       // This is what happens when the link is clicked
       e.preventDefault(); // Prevent the link from leaving the page
       $(this).addClass('active'); // Mark it active
       $(this).parent().siblings().children().removeClass('active'); // Remove active class from any other link
-      createPage(index, navObj.entries, navObj.matches); // Change the page
+      createPage(pageIndex, navObj.entries, navObj.matches); // Change the page
     });
-    // If this page is the current page, mark the link as active
-    navObj.page === index ? pageLink.children().addClass('active') : null;
     return pageLink;
   }
 
 
-  /*
-   * createPageNavList method
-   * Used by createPage
-   */
-
+ /*
+  * createPageNavList method
+  * Used by createPage
+  *
+  * @param  navObj: Keys: Page
+  *                       entries
+  *                       matches
+  *
+  * @returns DOM unordered list element for a page navigation
+  *
+  */
   var createPageNavList = function(navObj) {
     var navList = $('<ul></ul>'); // Create nav list.
     var navIndex = Math.ceil(navObj.matches.length / 10); // Get the number of pages needed.
@@ -129,12 +211,17 @@ var PageTurner = (function($) {
 
   /*
    * createPage method
-   * Used by Search, createPageNavList
-   * and Initialization code
+   * Used by Search, createPageLink Initialization code
+   *
+   * @param pageIndex: Integer - Page number you want to display (starts at 0)
+   *        entries: DOM element array - all possible items
+   *        matches: DOM element array - all currently matching search items
+   *
+   * @returns DOM object with pages and navigation attached
+   *
    */
-
-  var createPage = function(page, entries, matches) {
-    var navObj = displayPage(page, entries, matches);
+  var createPage = function(pageIndex, entries, matches) {
+    var navObj = displayPage(pageIndex, entries, matches);
     var navList = createPageNavList(navObj);
     var pagination = $(paginationString);
     pagination.append(navList);
@@ -142,39 +229,6 @@ var PageTurner = (function($) {
   }
 
 
-  function PageTurner(config) {
-
-    if(!config) {config = {};}
-    studentSearch = config.studentSearch || $('<div class="student-search"></div>');
-    pageHeader = config.pageHeader || $('.page-header');
-    currentPage = config.currentPage || $('.page');
-    students = config.students || $('.student-item');
-    empty = config.empty || $('<h1 class="empty">No Results Found</h1>');
-    studentList = config.studentList || $('.student-list');
-    input = config.input || $('<input placeholder="Search for students...">');
-    elementsToQuery = config.elementsToQuery || ['h3','span.email'];
-    itemsOnPage = config.itemsOnPage || 10;
-    paginationClass = config.paginationElement || 'pagination';
-    paginationElement = config.paginationElement || 'div';
-    paginationString = '<' + paginationElement + ' class="' + paginationClass + '"></' + paginationElement + '>';
-
-  }
-
-  /*
-   * Run method
-   * Used to attach PageTurner object to DOM
-   * and Initialization code
-   */
-
-  pageTurner.prototype.run =  function() {
-    var that = this;
-    input.keyup(search.bind(that));
-    studentSearch.append(input);
-    pageHeader.append(studentSearch);
-    var pagination = createPage(0, students, students);
-    currentPage.append(pagination);
-  }
-
   return pageTurner;
 
-})($);
+})($, window, document);
